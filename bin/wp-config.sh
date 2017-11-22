0992cc8e159612ae9681128dc52dde5b0b17b9b4
Dorian Logan <dlogan@brighttalk.com>
2017-11-22 14:53:52 +0000

    config script fix

--- bin/wp-config.sh
+++ bin/wp-config.sh
@@ -6,24 +6,22 @@ if [ -e wp-config-sample.php ]; then
 // Cache all the things \
 global $memcached_servers; \
 $memcached_servers = array( \
-	array( "memcached", "11211" ) \
+	array( \
+		"memcached", \
+		"11211" \
+		) \
 	); \
-  \
+// Allow LB to terminate SSL connection \
+$_SERVER["HTTPS"] = "on"; \
+$_SERVER["SERVER_PORT"] = 443; \
+// Allow WP to connect to web \
+define("FS_METHOD", "direct"); \
 // Proxy Server Settings \
 define("WP_PROXY_HOST", "proxy.gr.brighttalk.com"); \
 define("WP_PROXY_PORT", "443"); \
 define("WP_PROXY_USERNAME", ""); \
 define("WP_PROXY_PASSWORD", ""); \
 define("WP_PROXY_BYPASS_HOSTS", "localhost"); \
-  \ 
-// Allow LB to terminate SSL connection \
-$_SERVER["HTTPS"] = "on"; \
-$_SERVER["SERVER_PORT"] = 443; \
-#define("FORCE_SSL_ADMIN", true); \
-  \
-// Allos WP to connect to web \
-define("FS_METHOD", "direct"); \
-  \
-define( "WP_CACHE", true );' wp-config-sample.php 
+define( "WP_CACHE", true );' wp-config-sample.php
 
 fi
