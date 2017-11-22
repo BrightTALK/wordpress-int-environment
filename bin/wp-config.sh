2791fed2ee3a5cd88f83f0f14699e436a7c4a79e
Dorian Logan <dlogan@brighttalk.com>
2017-11-22 14:43:18 +0000

    added brighttalk proxy settings

--- bin/wp-config.sh
+++ bin/wp-config.sh
@@ -6,11 +6,24 @@ if [ -e wp-config-sample.php ]; then
 // Cache all the things \
 global $memcached_servers; \
 $memcached_servers = array( \
-	array( \
-		"memcached", \
-		"11211" \
-		) \
+	array( "memcached", "11211" ) \
 	); \
-define( "WP_CACHE", true );' wp-config-sample.php
+  \
+// Proxy Server Settings \
+define("WP_PROXY_HOST", "proxy.gr.brighttalk.com"); \
+define("WP_PROXY_PORT", "443"); \
+define("WP_PROXY_USERNAME", ""); \
+define("WP_PROXY_PASSWORD", ""); \
+define("WP_PROXY_BYPASS_HOSTS", "localhost"); \
+  \ 
+// Allow LB to terminate SSL connection \
+$_SERVER["HTTPS"] = "on"; \
+$_SERVER["SERVER_PORT"] = 443; \
+#define("FORCE_SSL_ADMIN", true); \
+  \
+// Allos WP to connect to web \
+define("FS_METHOD", "direct"); \
+  \
+define( "WP_CACHE", true );' wp-config-sample.php 
 
 fi
